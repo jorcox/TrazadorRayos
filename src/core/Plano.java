ddd1c1c30ec0e34cfbaec4ae9ac841470fc7628f
jorcox <646122@unizar.es>
2015-11-16 19:16:26 +0100

    Triángulo y Plano acabadicos

--- src/core/Plano.java
+++ src/core/Plano.java
@@ -6,7 +6,6 @@ import javax.vecmath.Vector3d;
 public class Plano extends Objeto{
 	
 	private Point3d p;
-	private Point3d p0;
 	private Vector3d n;
 	
 	public Plano (Point3d p, Vector3d n, Color color) {
@@ -20,7 +19,29 @@ public class Plano extends Objeto{
 	}
 
 	public Point3d interseccion(Rayo r) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector3d d = r.getD();
+		Point3d p = this.p;
+		double inf = d.dot(this.n);
+		if (inf == 0) {
+			/*
+			 * El rayo no intersecta, se devuelve null
+			 */
+			return null;
+		} else if (inf > 0) {
+			/*
+			 * El plano esta de espaldas al ojo, suponiendo que solo
+			 * se ver por una cara, entonces este no se ve
+			 */
+			return null;
+		} else {
+			/*
+			 * La normal del plano y el rayo van en sentidos inveros, entonces 
+			 * intersectan
+			 */
+			p.sub(r.getP0());
+			Vector3d vec = new Vector3d(p);
+			double sup = vec.dot(n);
+			return r.getPunto(sup/inf);
+		}
 	}
 }
--- src/core/Triangulo.java
+++ src/core/Triangulo.java
@@ -1,18 +1,58 @@
 package core;
 
 import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
 
 public class Triangulo extends Objeto{
+	private Point3d p1;
+	private Point3d p2;
+	private Point3d p3;
+	private Vector3d n;
+	
 
-	public Triangulo(Color color) {
+	public Triangulo(Point3d p1, Point3d p2, Point3d p3, Color color) {
 		super(color);
-		/* Seguir aqui con cosas propias del triangulo */
+		this.p1 = p1;
+		this.p2 = p2;
+		this.p3 = p3;
+		/*
+		 * Calculo de la normal n
+		 */
+		Point3d aux2 = p2;
+		Point3d aux3 = p3;
+		aux2.sub(p1);
+		aux3.sub(p1);
+		Vector3d vec12 = new Vector3d(aux2);
+		Vector3d vec13 = new Vector3d(aux3);
+		n.cross(vec12, vec13);
 	}
 	
 	@Override
 	public Point3d interseccion(Rayo r) {
-		// TODO Auto-generated method stub
-		return null;
+		Vector3d d = r.getD();
+		Point3d p = this.p1;
+		double inf = d.dot(this.n);
+		if (inf == 0) {
+			/*
+			 * El rayo no intersecta, se devuelve null
+			 */
+			return null;
+		} else if (inf > 0) {
+			/*
+			 * El triangulo esta de espaldas al ojo, suponiendo que solo
+			 * se ver por una cara, entonces este no se ve
+			 */
+			return null;
+		} else {
+			/*
+			 * La normal del triangulo y el rayo van en sentidos inveros, entonces 
+			 * intersectan
+			 */
+			p.sub(r.getP0());
+			Vector3d vec = new Vector3d(p);
+			double sup = vec.dot(n);
+			return r.getPunto(sup/inf);
+		}
 	}
 
 }
