7f55e4c23de78cf843e3ef351a32350c0b4a7a27
jorcox <646122@unizar.es>
2015-12-08 12:34:33 +0100

    dsfdsf

--- src/core/Trazador.java
+++ src/core/Trazador.java
@@ -20,7 +20,7 @@ public class Trazador {
 	private static ArrayList<Objeto> objetos = new ArrayList<Objeto>();
 	private static Camara camara = null;
 	private static ArrayList<Luz>  luces = null;
-	private static double iAmbiental = 0.1;
+	private static double iAmbiental = 0.0;
 	private static final int NUM_ANTIALIASING = 1;
 	private static final String NOMBRE_IMG = "imagen";
 	private static final String FORMATO_IMG = "png";
